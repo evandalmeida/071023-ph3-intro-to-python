4d35a8b1819d17cd13ace95b51a005c491af56c7
brewchetta <chett.tiller@gmail.com>
2023-08-16 13:43:56 -0400

    init

---
+++ 02-conditional.py
@@ -0,0 +1,20 @@
+# let name = "Bob"
+
+# if (name === "Bob") {
+#     console.log("OMG it's Bob!")
+# } else {
+#     console.log(`Hello ${name} the raccoon`)
+# }
+
+# name = "Raphael"
+
+# if (name === "Donatello" || name === "Raphael") {
+#     console.log("Cowabunga dude!")
+# } else if (name === "Leonardo" || name === "Michelangelo") {
+#     console.log("It's turtle time!")
+# }
+
+# console.log("Free Pineapple Pizza on Tuesdays!")
+
+# let weekday = "Tuesday"
+# let pizza = weekday === "Tuesday" ? "Pineapple Pizza" : "Pepperoni Pizza"
---
+++ README.md
@@ -0,0 +1,5 @@
+# Intro to Python
+
+For this workshop you'll be translating Javascript syntax into Python. There are several sections with comments that show Javascript code. Below each comment we'll be building analogous Python code.
+
+You may run a file with `python3 01-variables.py` replacing 01-variables.py with whatever you'd prefer to test.
